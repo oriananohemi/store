7507ee602d01521e97f6b848e26d695e0d1543c9
Oriana <oriana.nohemi22@gmail.com>
2021-04-21 21:53:39 -0500

    componente Home

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faShoppingCart,faChevronDown, faChevronLeft, faChevronRight, faSearch, faTrashAlt, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
+import Home from './pages/Home'
+
 library.add(fab, faShoppingCart, faChevronDown, faChevronLeft, faChevronRight, faSearch, faTrashAlt, faPlus, faMinus)
 
 function App() {
   return (
     <div className="App">
+      <Home/>
     </div>
   );
 }
