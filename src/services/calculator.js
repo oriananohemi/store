c798d3081855790cf86b26d0d4ebda580c926dac
Oriana <oriana.nohemi22@gmail.com>
2021-04-23 00:20:39 -0500

    logica para eliminar, agregar o finalizar compra dependiendo del parametro recibido

--- src/services/calculator.js
+++ src/services/calculator.js
@@ -5,5 +5,33 @@ export const editCart = (product, action) => {
         if(!newProduct) {
             return currentCart.concat({...product, quantity: 1})
         }
+
+        currentCart.reduce((copieCart, item) => {
+            if(product.id !== item.id) {
+                return copieCart.concat(item)
+            }
+
+            switch(action) {
+                case "decrement": {
+                    if(item.quantity === 1) {
+                        return copieCart
+                    }
+
+                    return copieCart.concat({...product, quantity: item.quantity -1})
+                }
+                case "removeProduct": {
+                    return copieCart   
+                }
+                case "increment": {
+                    return copieCart.concat({...product, quantity: item.quantity + 1})
+                }
+                case "finalized": {
+                    return {}
+                }
+                default: {
+                    return copieCart.concat(item)
+                }
+            }
+        }, [])
     }
 }
