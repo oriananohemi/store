285bb741fbe339f53d51ac0de00b82382f1dd498
Oriana <oriana.nohemi22@gmail.com>
2021-04-23 07:27:49 -0500

    agrega estilos para header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../assets/images/brand.png';
 import profile from '../assets/images/profile.png';
 
 
 const Header = () => {
+  const [navVisible, setNavVisible] = useState(true)
+
+  const toggleNav = () => {
+    setNavVisible(!navVisible)
+  }
     return(
       <header className="header">
         <img className="header__brand"  src={logo} alt="Logo Superfüds"/>
-        <div className="input-container">
-          <input className="input" type="text" name="" id="" icon="check-square"/>
-          <FontAwesomeIcon className="input-icon" icon="search" />
-        </div>
-        
-        <FontAwesomeIcon className="header__shoppingCart fa-lg" icon="shopping-cart" />
-
-        <div className="flex-container">
-          <div>
-            <h2 className="header__profile">Saiby Alimentos</h2>
-            <div className="flex-container">
-              <h3>Mi perfil</h3>
-              <FontAwesomeIcon icon="chevron-down" />
+        {
+          navVisible && (
+            <div className="header-subcontainer">
+              <div className="input-container">
+                <input className="input" type="text" icon="check-square" placeholder="Busca marcas y productos..."/>
+                <FontAwesomeIcon className="input-icon" icon="search" />
+              </div>
+              
+              <FontAwesomeIcon className="header__shoppingCart fa-lg" icon="shopping-cart" />
+    
+              <div className="flex-container header__options">
+                <div>
+                  <h2 className="header__profile">Saiby Alimentos</h2>
+                  <div className="flex-container">
+                    <h3>Mi perfil</h3>
+                    <FontAwesomeIcon icon="chevron-down" />
+                  </div>
+                  {/* <nav className="header__profile__options">
+                    <ul>
+                      <li role="list">
+                        <a>Mis compras</a>
+                      </li>
+                      <li role="list">
+                        <a>Configuración</a>
+                      </li>
+                      <li role="list">
+                        <a>Cerrar Sesión</a>
+                      </li>
+                    </ul>
+                  </nav> */}
+                </div>
+                <img className="header__profile-image" src={profile} alt=""/>
+              </div>
             </div>
-          </div>
-          <img className="header__profile-image" src={profile} alt=""/>
-        </div>
+          )
+        }
+        <FontAwesomeIcon onClick={toggleNav} className="header__responsive-icon fa-lg" icon="bars" />
       </header>
     )
 }
