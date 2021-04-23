4c39b148a80984b700e132e7026f962b816442c1
Oriana <oriana.nohemi22@gmail.com>
2021-04-23 08:52:55 -0500

    icono para header responsive

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './styles/App.scss';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faShoppingCart,faChevronDown, faChevronLeft, faChevronRight, faSearch, faTrashAlt, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart,faChevronDown, faChevronLeft, faChevronRight, faSearch, faTrashAlt, faPlus, faMinus, faBars } from '@fortawesome/free-solid-svg-icons'
 
 import Home from './pages/Home'
 
-library.add(fab, faShoppingCart, faChevronDown, faChevronLeft, faChevronRight, faSearch, faTrashAlt, faPlus, faMinus)
+library.add(fab, faShoppingCart, faChevronDown, faChevronLeft, faChevronRight, faSearch, faTrashAlt, faPlus, faMinus, faBars)
 
 function App() {
   return (
