d293f55d434ac94474b77f069a35e1f5c5fdbc91
Oriana <oriana.nohemi22@gmail.com>
2021-04-23 10:25:15 -0500

    transicion para el header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { CSSTransition } from "react-transition-group";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../assets/images/brand.png';
@@ -7,49 +9,58 @@ import profile from '../assets/images/profile.png';
 
 const Header = () => {
   const [navVisible, setNavVisible] = useState(true)
+  const [smallScreen, setSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setSmallScreen(true);
+    } else {
+      setSmallScreen(false);
+    }
+  };
 
   const toggleNav = () => {
     setNavVisible(!navVisible)
   }
     return(
       <header className="header">
-        <img className="header__brand"  src={logo} alt="Logo Superfüds"/>
-        {
-          navVisible && (
-            <div className="header-subcontainer">
-              <div className="input-container">
-                <input className="input" type="text" icon="check-square" placeholder="Busca marcas y productos..."/>
-                <FontAwesomeIcon className="input-icon" icon="search" />
-              </div>
-              
-              <FontAwesomeIcon className="header__shoppingCart fa-lg" icon="shopping-cart" />
-    
-              <div className="flex-container header__options">
-                <div>
-                  <h2 className="header__profile">Saiby Alimentos</h2>
-                  <div className="flex-container">
-                    <h3>Mi perfil</h3>
-                    <FontAwesomeIcon icon="chevron-down" />
-                  </div>
-                  {/* <nav className="header__profile__options">
-                    <ul>
-                      <li role="list">
-                        <a>Mis compras</a>
-                      </li>
-                      <li role="list">
-                        <a>Configuración</a>
-                      </li>
-                      <li role="list">
-                        <a>Cerrar Sesión</a>
-                      </li>
-                    </ul>
-                  </nav> */}
-                </div>
-                <img className="header__profile-image" src={profile} alt=""/>
+        <img className="header__brand" src={logo} alt="Logo Superfüds"/>
+        <CSSTransition
+        in={!smallScreen || navVisible}
+        timeout={350}
+        classNames="headerAnimation"
+        unmountOnExit
+      >
+        <div className="header-subcontainer">
+          <div className="input-container">
+            <input className="input" type="text" icon="check-square" placeholder="Busca marcas y productos..."/>
+            <FontAwesomeIcon className="input-icon" icon="search" />
+          </div>
+          
+          <FontAwesomeIcon className="header__shoppingCart fa-lg" icon="shopping-cart" />
+
+          <div className="flex-container header__options">
+            <div>
+              <h2 className="header__profile">Saiby Alimentos</h2>
+              <div className="flex-container">
+                <h3>Mi perfil</h3>
+                <FontAwesomeIcon icon="chevron-down" />
               </div>
             </div>
-          )
-        }
+            <img className="header__profile-image" src={profile} alt=""/>
+          </div>
+        </div>
+          </CSSTransition>
         <FontAwesomeIcon onClick={toggleNav} className="header__responsive-icon fa-lg" icon="bars" />
       </header>
     )
